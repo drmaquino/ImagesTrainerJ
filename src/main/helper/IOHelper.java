3fc9cdf20075b5053a6dd063a6810967729e4ad6
Mariano Aquino (e22138) <Mariano.Aquino@cmegroup.com>
2014-11-25 17:20:20 -0300

    agrego metodos de creacion de carpeta y recuperacion de imagenes

--- src/main/helper/IOHelper.java
+++ src/main/helper/IOHelper.java
@@ -3,7 +3,8 @@ package main.helper;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -32,7 +33,7 @@ public class IOHelper
 		this._context = context;
 		this._externalStoragePath = Environment.getExternalStorageDirectory().toString();
 	}
-	
+
 	public void createGameFolder()
 	{
 		File file = null;
@@ -51,13 +52,14 @@ public class IOHelper
 			Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
 		}
 	}
-	
+
 	public void saveToFile(File file, CharSequence texto, boolean append)
 	{
 		try
 		{
 			FileOutputStream fos = new FileOutputStream(file, append);
-			//esto deberia hacer que se puedan grabar caracteres utf8 pero no funciona
+			// esto deberia hacer que se puedan grabar caracteres utf8 pero no
+			// funciona
 			OutputStreamWriter myOutWriter = new OutputStreamWriter(fos);
 			myOutWriter.append(texto);
 			myOutWriter.append("\n");
@@ -79,6 +81,24 @@ public class IOHelper
 			myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
 		}
 		return myBitmap;
+	}
+
+	public List<String> getListImagesInGameFolder()
+	{
+		List<String> files = new ArrayList<String>();
+		File fileList = new File(String.format("%s/%s/", _externalStoragePath, IMAGES_FOLDER));
+		if (fileList != null)
+		{
+			File[] filenames = fileList.listFiles();
+			for (File tmpf : filenames)
+			{
+				if (tmpf.getName().endsWith(".jpg"))
+				{
+					files.add(tmpf.getName());
+				}
+			}
+		}
+		return files;
 	}
 
 	private Context getBaseContext()
