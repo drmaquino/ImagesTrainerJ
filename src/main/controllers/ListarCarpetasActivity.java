d2dedbdd668187f4f8a0702273a96980dd0a9007
mariano <mariano.aquino@gmail.com>
2014-12-06 01:06:03 -0300

    agrego dialogo about provisorio

--- src/main/controllers/ListarCarpetasActivity.java
+++ src/main/controllers/ListarCarpetasActivity.java
@@ -5,6 +5,9 @@ import java.util.List;
 import main.helper.DBHelper;
 import main.helper.IOHelper;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -22,78 +25,100 @@ import android.widget.Toast;
 
 public class ListarCarpetasActivity extends Activity
 {
-	private ListView lvCarpetas;
-	private IOHelper ioh;
-	protected DBHelper dbh;
-	List<String> carpetas;
+    private ListView lvCarpetas;
+    private IOHelper ioh;
+    protected DBHelper dbh;
+    List<String> carpetas;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_listar_carpetas);
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_carpetas);
 
-		lvCarpetas = (ListView) findViewById(R.id.listaDeCarpetas);
+        lvCarpetas = (ListView) findViewById(R.id.listaDeCarpetas);
 
-		ioh = new IOHelper(this);
-		dbh = new DBHelper(this);
+        ioh = new IOHelper(this);
+        dbh = new DBHelper(this);
 
-		ioh.createGameFolder();
+        ioh.createGameFolder();
 
-		carpetas = ioh.getListFoldersInGameFolder();
+        carpetas = ioh.getListFoldersInGameFolder();
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, carpetas)
-		{
-			@Override
-			public View getView(int position, View convertView, ViewGroup parent)
-			{
-				View view = super.getView(position, convertView, parent);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, carpetas)
+        {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent)
+            {
+                View view = super.getView(position, convertView, parent);
 
-				TextView textView = (TextView) view.findViewById(android.R.id.text1);
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
 
-				/* YOUR CHOICE OF COLOR */
-				textView.setTextColor(Color.WHITE);
+                /* YOUR CHOICE OF COLOR */
+                textView.setTextColor(Color.WHITE);
 
-				return view;
-			}
-		};
-		lvCarpetas.setAdapter(adapter);
+                return view;
+            }
+        };
+        lvCarpetas.setAdapter(adapter);
 
-		lvCarpetas.setOnItemClickListener(new OnItemClickListener()
-		{
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id)
-			{
-				String carpeta = carpetas.get(position);
-				Toast.makeText(getApplicationContext(), "sincronizando \"" + carpeta + "\"...", Toast.LENGTH_SHORT).show();
-				dbh.sincronizarCarpeta(carpeta);
-				Intent intent = new Intent(getApplicationContext(), ListarJuegosActivity.class);
-				intent.putExtra("carpeta", carpeta);
-				startActivity(intent);
-			}
-		});
-	}
+        lvCarpetas.setOnItemClickListener(new OnItemClickListener()
+        {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                String carpeta = carpetas.get(position);
+                Toast.makeText(getApplicationContext(), "sincronizando \"" + carpeta + "\"...", Toast.LENGTH_SHORT).show();
+                dbh.sincronizarCarpeta(carpeta);
+                Intent intent = new Intent(getApplicationContext(), ListarJuegosActivity.class);
+                intent.putExtra("carpeta", carpeta);
+                startActivity(intent);
+            }
+        });
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
-		{
-			case R.id.action_help:
-				Toast.makeText(this, "help...", Toast.LENGTH_SHORT).show();
-				return true;
-			case R.id.action_about:
-				Toast.makeText(this, "about...", Toast.LENGTH_SHORT).show();
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-		}
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_about:
+                crearDialogoAbout().show();
+                return true;
+            case R.id.action_help:
+                Toast.makeText(this, "help...", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private AlertDialog crearDialogoAbout()
+    {
+        Builder dbAbout = new AlertDialog.Builder(this);
+        dbAbout.setTitle("Images Trainer");
+        String msg = "";
+        msg += "\nCreado por:";
+        msg += "\nMariano Aquino";
+        msg += "\n";  
+        msg += "\nVersión 1.0";      
+        msg += "\n";
+        msg += "\n06/12/2014";
+        dbAbout.setMessage(msg);
+        dbAbout.setNeutralButton("Cerrar", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int whichButton)
+            {
+                dialog.dismiss();
+            }
+        });
+        return dbAbout.create();
+    }
 }
