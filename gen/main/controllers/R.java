abaf6b11ba2d3cfd9a86ce5e3e4cfa1a8047f0d4
mariano <mariano.aquino@gmail.com>
2014-12-23 16:28:31 -0800

    agrego spinners a la pantalla de elegir modo de practica

--- gen/main/controllers/R.java
+++ gen/main/controllers/R.java
@@ -8,6 +8,9 @@
 package main.controllers;
 
 public final class R {
+    public static final class array {
+        public static final int country_arrays=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -27,23 +30,25 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_about=0x7f08000f;
-        public static final int action_help=0x7f08000e;
-        public static final int action_refresh_db=0x7f080010;
-        public static final int btn1=0x7f080008;
-        public static final int btn2=0x7f080009;
-        public static final int btn3=0x7f08000a;
-        public static final int btn4=0x7f08000b;
-        public static final int current_image=0x7f080002;
-        public static final int current_pair=0x7f080007;
-        public static final int current_pendientes=0x7f080001;
-        public static final int current_resueltas=0x7f080000;
-        public static final int dialogButtonOK=0x7f08000d;
-        public static final int listaDeCarpetas=0x7f08000c;
-        public static final int pair_1=0x7f080003;
-        public static final int pair_2=0x7f080004;
-        public static final int pair_3=0x7f080005;
-        public static final int pair_4=0x7f080006;
+        public static final int action_about=0x7f090011;
+        public static final int action_help=0x7f090010;
+        public static final int action_refresh_db=0x7f090012;
+        public static final int btn1=0x7f090008;
+        public static final int btn2=0x7f090009;
+        public static final int btn3=0x7f09000a;
+        public static final int btn4=0x7f09000b;
+        public static final int current_image=0x7f090002;
+        public static final int current_pair=0x7f090007;
+        public static final int current_pendientes=0x7f090001;
+        public static final int current_resueltas=0x7f090000;
+        public static final int dialogButtonOK=0x7f09000f;
+        public static final int listaDeCarpetas=0x7f09000c;
+        public static final int pair_1=0x7f090003;
+        public static final int pair_2=0x7f090004;
+        public static final int pair_3=0x7f090005;
+        public static final int pair_4=0x7f090006;
+        public static final int spinner1=0x7f09000d;
+        public static final int spinner2=0x7f09000e;
     }
     public static final class layout {
         public static final int activity_decoding=0x7f030000;
@@ -53,11 +58,12 @@ public final class R {
         public static final int dialog_custom=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int country_prompt=0x7f050007;
         public static final int default_description=0x7f050006;
         public static final int title_activity_decoding=0x7f050005;
         public static final int title_activity_encoding=0x7f050004;
@@ -89,10 +95,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
